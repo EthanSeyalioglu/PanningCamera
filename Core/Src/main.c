92abda038f5910b42c33713e3e1f73a33264a306
Seyaboy <seyacocuk@gmail.com>
2026-07-09 17:49:06 -0700

    improve readability

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -5,90 +5,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define CENTER      512
-#define DEADZONE   10
+#define THUMBSTICK_CENTER       512
+#define THUMBSTICK_DEADZONE     20
 
-uint8_t position;
-uint16_t pulse_width;
-int thumbstick_values[2];
-uint8_t direction;
+#define SERVO_MIN               500
+#define SERVO_CENTER            1400
+#define SERVO_MAX               2400
+#define SERVO_STEP              100
+
+typedef enum
+{
+    LEFT = -1,
+    CENTER = 0,
+    RIGHT = 1
+} ServoDirection;
 
 uint16_t get_pulse_width(uint8_t position);
-void get_servo_direction(int thumbstick_val_x);
+ServoDirection thumbstick_get_direction(int thumbstick_val_x);
 
 int main(void)
 {
-  uart_init();
-  uart_tx_start();
-  printf("UART Initialized\n\n");
+    uint16_t pulse_width;
+    int thumbstick_values[2];
+    ServoDirection direction;
 
-  adc_thumbstick_init();
-  adc_thumbstick_config();
-  printf("ADC Initialized\n\n");
+    uart_init();
+    uart_tx_start();
+    printf("UART Initialized\n\n");
 
-  tim2_pwm_init();
-  tim2_servo_pwm_config();
-  tim2_pwm_start();
-  printf("PWM Initialized\n\n");
+    adc_thumbstick_init();
+    adc_thumbstick_config();
+    printf("ADC Initialized\n\n");
+
+    tim2_pwm_init();
+    tim2_servo_pwm_config();
+    tim2_pwm_start();
+    printf("PWM Initialized\n\n");
 
 
-  pulse_width = 1400;
+    pulse_width = SERVO_CENTER;
 
-  while (1)
-  {
-
-    adc_thumbstick_scan();
-    adc_thumbstick_read(thumbstick_values);
-
-    get_servo_direction(thumbstick_values[0]);
-
-    if (direction == 2)
+    while (1)
     {
-        pulse_width += 100;
-        pulse_width = pulse_width > 2400 ? 2400 : pulse_width; // min
+        adc_thumbstick_scan();
+        adc_thumbstick_read(thumbstick_values);
+
+        direction = thumbstick_get_direction(thumbstick_values[0]);
+
+        if (direction == RIGHT)
+        {
+            pulse_width += SERVO_STEP;
+            pulse_width = (pulse_width > SERVO_MAX ? SERVO_MAX : pulse_width); // 2400 ceiling
+        }
+        else if (direction == LEFT)
+        {
+            pulse_width -= SERVO_STEP;
+            pulse_width = (pulse_width < SERVO_MIN ? SERVO_MIN : pulse_width); // 600 floor
+        }
+
+        servo_set_position(pulse_width);
+
+        for (int i = 0; i < 10000; i++) {}
+
+        // printf("Pulse Width: %d\n", pulse_width);
+
+        // if (pulse_width > 2500 || pulse_width < 500)
+        // {
+        //     polarity = (polarity - 1) * -1;
+        // }
+
+        printf("Pulse Width: %d\n", pulse_width);
+        printf("Thumbstick X: %d\n", thumbstick_values[0]);
+        printf("Servo Direction: %d\n\n", direction);
+        // printf("Thumbstick X: %d\nThumbstick Y: %d\n\n", thumbstick_values[0], thumbstick_values[1]);
     }
-    else if (direction == 0)
-    {
-        pulse_width -= 100;
-        pulse_width = pulse_width < 100 ? 100 : pulse_width; // max
-    }
-
-    servo_set_position(pulse_width);
-
-    for (int i = 0; i < 10000; i++) {}
-
-    // printf("Pulse Width: %d\n", pulse_width);
-
-    // if (pulse_width > 2500 || pulse_width < 500)
-    // {
-    //     polarity = (polarity - 1) * -1;
-    // }
-
-    printf("Pulse Width: %d\n", pulse_width);
-    printf("Thumbstick X: %d\n", thumbstick_values[0]);
-    printf("Servo Direction: %d\n\n", direction);
-    // printf("Thumbstick X: %d\nThumbstick Y: %d\n\n", thumbstick_values[0], thumbstick_values[1]);
-  }
 }
 
 // pulse width 500 is starting position, 2500 is ending
 
 
-void get_servo_direction(int thumbstick_val_x)
+ServoDirection thumbstick_get_direction(int thumbstick_val_x)
 {
-    if (abs(thumbstick_val_x - CENTER) > DEADZONE)
+    if (abs(thumbstick_val_x - THUMBSTICK_CENTER) > THUMBSTICK_DEADZONE)
     {
-        if (thumbstick_val_x >= 512)
-        {
-            direction = 2;
-        }
-        else
-        {
-            direction = 0;
-        }
+        return (thumbstick_val_x >= THUMBSTICK_CENTER ? RIGHT : LEFT);
     }
     else
     {
-        direction = 1;
+        return CENTER;
     }
 }
